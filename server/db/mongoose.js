a3e451cc7b23b34c3e98806fff2fad06b7b1c48c
Christian da Silva <cps@oxyguard.dk>
2018-09-28 15:00:13 +0200

    Fix for heroku connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,3 +6,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 module.exports = {
     mongoose
 };
+
+// config:set MONGODB_URI="mongodb://cnps:Marley1543@ds253918.mlab.com:53918/learning-app"
