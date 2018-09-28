8e749652c40ee0c6788e6caa341e43880933fb1e
Christian da Silva <cps@oxyguard.dk>
2018-09-28 14:52:04 +0200

    Fix bug in mongoose connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://cnps:Marley1543@ds215563.mlab.com:15563/learning-app');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
