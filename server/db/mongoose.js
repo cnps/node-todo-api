02c75b0a3cfd5c737b1cd8426e34ed8f3dd35c76
Christian da Silva <cps@oxyguard.dk>
2018-09-28 14:27:31 +0200

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://cnps:Marley1543@ds215563.mlab.com:15563/learning-app');
 
 module.exports = {
     mongoose
