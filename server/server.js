331c732ab3aef71aac9e58a3fe7e00d7ef8a68de
Christian da Silva <cps@oxyguard.dk>
2018-09-28 11:39:43 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +27,25 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+      return  res.status(404).send('404 - Object id not valid'); 
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.send({todo});
+        } else {
+            return res.status(404).send('404 - Object id not found');
+        }
+    }, (e) => {
+        res.status(400).send('400 - error');
+        
     })
 });
 
