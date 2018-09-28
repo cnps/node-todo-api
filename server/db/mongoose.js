dce2d397ea6afc8874595aa968b5a74b58d2f8d9
Christian da Silva <cps@oxyguard.dk>
2018-09-28 15:45:45 +0200

    fix another try

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,16 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-if (process.env.MONGODB_URI) {
-    connectPath = "mongodb://cnps:Marley1543@ds215563.mlab.com:15563/learning-app"
-} else {
-    connectPath = 'mongodb://localhost:27017/TodoApp'
-}
-mongoose.connect(connectPath);
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://cnps:Marley1543@ds215563.mlab.com:15563/learning-app'
+  };
+  mongoose.connect( db.localhost || db.mlab);
 
 module.exports = {
     mongoose
 };
-
-// config:set MONGODB_URI="mongodb://cnps:Marley1543@ds215563.mlab.com:15563/learning-app"
-mongodb://<dbuser>:<dbpassword>@ds215563.mlab.com:15563/learning-app
