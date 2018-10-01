47339d543ddc10298cebc838b3d64bea40393557
Christian da Silva <cps@oxyguard.dk>
2018-10-01 10:38:47 +0200

    should be fixed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: process.env.MONGODB_URI
-  };
-  mongoose.connect( db.mlab );
-console.log(db);
+mongoose.connect( process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp' );
 
 module.exports = {
     mongoose
