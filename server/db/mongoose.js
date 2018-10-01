5be9a202b7f00871ff8963c46178ab9edaba2936
Christian da Silva <cps@oxyguard.dk>
2018-10-01 10:34:39 +0200

    again

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
     mlab: process.env.MONGODB_URI
   };
-  mongoose.connect( db.localhost || db.mlab );
+  mongoose.connect( db.mlab );
 console.log(db);
 
 module.exports = {
